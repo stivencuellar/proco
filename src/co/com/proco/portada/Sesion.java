dde19a1ec5b2322868e087b3752979a2c0ce44aa
SCuellar <stiven.cuellar@onesystem.com.co>
2015-09-08 07:37:29 -0500

    Se agregan atributos a sesión y propiedades

--- src/co/com/proco/portada/Sesion.java
+++ src/co/com/proco/portada/Sesion.java
@@ -1,5 +1,38 @@
 package co.com.proco.portada;
 
+/**
+ * 
+ * @author SCuellar
+ *
+ */
 public class Sesion {
+	
+	/**
+	 * Titulo de la sesión
+	 */
+	private String titulo;
+	
+	/**
+	 * Imagen usada en la sesión
+	 */
+	private String imagen;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	
+	
 
 }
