37d3432765b9e90688a80d37931fb3ba18cc9857
ManuLoaJara <manuela@loaizas.com>
2015-09-10 06:36:39 -0500

    Controlador
    Creacion de controlador y creacion de sevlet para el manejo de la capa
    de la vista y los eventos by:cuellar

--- src/cp/com/hobbies/controller/SessionController.java
+++ src/cp/com/hobbies/controller/SessionController.java
@@ -1,7 +1,18 @@
 package cp.com.hobbies.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import co.com.proco.portada.Sesion;
 
+@Controller
 public class SessionController {
-
+	
+	@RequestMapping(value = "/session", method = RequestMethod.GET)
+	public ModelAndView inicioSesiones(){
+		ModelAndView modelAndView = new ModelAndView("index", "sesionobj", new Sesion());
+		return modelAndView;
+	}
 }
