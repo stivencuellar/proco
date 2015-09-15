1ddfdec958a0debc47a52afbf8c3c0a194d6d980
SCuellar <stiven.cuellar@onesystem.com.co>
2015-09-15 07:18:03 -0500

    Subiendo controlador

--- src/co/com/proco/controller/SessionController.java
+++ src/co/com/proco/controller/SessionController.java
@@ -1,6 +1,7 @@
 package co.com.proco.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,9 +11,19 @@ import co.com.proco.portada.Sesion;
 @Controller
 public class SessionController {
 	
-	@RequestMapping(value = "/session", method = RequestMethod.GET)
+	@RequestMapping(value = "/Session", method = RequestMethod.GET)
 	public ModelAndView inicioSesiones(){
 		ModelAndView modelAndView = new ModelAndView("index", "sesionobj", new Sesion());
 		return modelAndView;
 	}
+	
+	
+//	@RequestMapping(value = "/Session", method = RequestMethod.GET)
+//	public String printWelcome(ModelMap model) {
+//
+//		model.addAttribute("message", "Spring 3 MVC Hello World");
+//		return "hello";
+//
+//	}
+
 }
